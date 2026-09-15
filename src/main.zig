938f154dc278dfd9680d4215d89f950afc345b31
Mohammed Badri <mohammed.0805@hotmail.com>
2026-09-15 16:38:20 +0200

    Build output layout

--- src/main.zig
+++ src/main.zig
@@ -22,11 +22,11 @@ pub fn main(init: std.process.Init) !void {
 
     const count = values.object.count();
 
-    const obj = values.object;
+    const object_map = values.object;
 
     var compteurs = std.enums.EnumArray(safetensors.Dtype, u64).initFill(0);
 
-    for (obj.values()) |v| {
+    for (object_map.values()) |v| {
         if (v.object.get("dtype")) |d| {
             if (std.meta.stringToEnum(safetensors.Dtype, d.string)) |ds| {
                 compteurs.getPtr(ds).* += 1;
@@ -34,9 +34,24 @@ pub fn main(init: std.process.Init) !void {
         }
     }
 
+    //for (object_map.keys(), object_map.values()) |k, v| {
+    //    if (v.object.get("dtype")) |d| {
+    //        std.debug.print("{s} -> {f}\n", .{ k, std.json.fmt(v, .{}) });
+    //        _ = d;
+    //    }
+    //}
+
     var it = compteurs.iterator();
     while (it.next()) |e|
         std.debug.print("{s:<9} {d:>3}\n", .{ @tagName(e.key), e.value.* });
 
-    std.debug.print("total : {d}", .{count});
+    std.debug.print("total : {d}\n", .{count});
+
+    const out = try safetensors.plan(gpa, object_map);
+    defer gpa.free(out);
+
+    std.debug.print("out len : {d}", .{out.len});
+
+    const end_file = out[out.len - 1].fin;
+    std.debug.print("size from out : {d}", .{end_file});
 }
--- src/safetensors.zig
+++ src/safetensors.zig
@@ -17,6 +17,15 @@ pub const SafetensorsFile = struct {
     }
 };
 
+const Sortie = struct {
+    nom: []const u8,
+    dtype: Dtype,
+    rank: u8,
+    shape: [4]u64,
+    debut: u64,
+    fin: u64,
+};
+
 pub fn open_tensor(io: std.Io, gpa: std.mem.Allocator, filename: []const u8) ![]const u8 {
     const file: std.Io.File = try std.Io.Dir.openFile(.cwd(), io, filename, .{ .mode = .read_only });
     defer file.close(io);
@@ -49,3 +58,66 @@ pub fn open(io: std.Io, gpa: std.mem.Allocator, filename: []const u8) !Safetenso
 
     return safetensors_file;
 }
+
+pub fn plan(gpa: std.mem.Allocator, object_map: std.json.ObjectMap) ![]Sortie {
+    var res: std.ArrayList(Sortie) = .empty;
+
+    var cursor: u64 = 0;
+    for (object_map.keys(), object_map.values()) |k, v| {
+        if (!std.mem.endsWith(u8, k, "weight_scale") and !std.mem.endsWith(u8, k, "weight_scale_2")) {
+            if (v.object.get("dtype")) |d| {
+                var obj = Sortie{
+                    .nom = k,
+                    .dtype = std.meta.stringToEnum(Dtype, d.string).?,
+                    .rank = @intCast(v.object.get("shape").?.array.items.len),
+                    .shape = blk: {
+                        var t: [4]u64 = .{ 1, 1, 1, 1 };
+                        for (0..v.object.get("shape").?.array.items.len) |i| t[i] = @intCast(v.object.get("shape").?.array.items[i].integer);
+                        break :blk t;
+                    },
+                    .debut = cursor,
+                    .fin = undefined,
+                };
+                if (obj.dtype == .U8) {
+                    obj.dtype = .F32;
+                    obj.shape[obj.rank - 1] = obj.shape[obj.rank - 1] * 2;
+                }
+
+                var factor: u64 = 0;
+                switch (obj.dtype) {
+                    .F32 => factor = 4,
+                    .BF16 => factor = 2,
+                    .F8_E4M3 => factor = 1,
+                    .U8 => {
+                        std.debug.print("a U8 value went through condition", .{});
+                    },
+                }
+
+                var shape_size: u64 = 1;
+                for (0..obj.rank) |i| {
+                    shape_size = shape_size * obj.shape[i];
+                }
+
+                obj.fin = cursor + shape_size * factor;
+                cursor += obj.fin - obj.debut;
+
+                try res.append(gpa, obj);
+            }
+        }
+    }
+
+    return res.toOwnedSlice(gpa);
+}
+
+//fn write(){
+//        try js.objectField(obj.nom);
+//    try js.beginObject();
+//    try js.objectField("dtype");
+//    try js.write(obj.dtype);
+//    try js.objectField("shape");
+//    try js.write(obj.shape);
+//    try js.objectField("data_offsets");
+//    try js.write(.{ obj.debut, obj.fin });
+//    try js.endObject();
+//
+//}
