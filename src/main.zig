86d692a523b4d1e024c44ef94324abee72f9b77a
Mohammed Badri <mohammed.0805@hotmail.com>
2026-09-15 02:03:23 +0200

    Read header function

--- src/main.zig
+++ src/main.zig
@@ -5,7 +5,38 @@ const safetensors = @import("safetensors.zig");
 pub fn main(init: std.process.Init) !void {
     const io = init.io;
     const gpa = init.gpa;
+    const arena = init.arena.allocator();
 
-    _ = io;
-    _ = gpa;
+    const args = try init.minimal.args.toSlice(arena);
+    if (args.len < 2) {
+        std.debug.print("Erreur : Veuillez passer au moins un argument.\n", .{});
+        return;
+    }
+
+    const safetensor_file = try safetensors.open(io, gpa, args[1]);
+    defer safetensor_file.deinit(io);
+
+    std.debug.print("count : {d}\n", .{safetensor_file.offset});
+
+    const values = safetensor_file.parsed.value;
+
+    const count = values.object.count();
+
+    const obj = values.object;
+
+    var compteurs = std.enums.EnumArray(safetensors.Dtype, u64).initFill(0);
+
+    for (obj.values()) |v| {
+        if (v.object.get("dtype")) |d| {
+            if (std.meta.stringToEnum(safetensors.Dtype, d.string)) |ds| {
+                compteurs.getPtr(ds).* += 1;
+            } else std.debug.print("Type non reconnu : {s}\n", .{d.string});
+        }
+    }
+
+    var it = compteurs.iterator();
+    while (it.next()) |e|
+        std.debug.print("{s:<9} {d:>3}\n", .{ @tagName(e.key), e.value.* });
+
+    std.debug.print("total : {d}", .{count});
 }
--- src/safetensors.zig
+++ src/safetensors.zig
@@ -1,5 +1,22 @@
 const std = @import("std");
 
+pub const Dtype = enum { U8, F8_E4M3, F32, BF16 };
+
+pub const SafetensorsFile = struct {
+    const Self = @This();
+    gpa: std.mem.Allocator,
+    file: std.Io.File,
+    data: []const u8,
+    parsed: std.json.Parsed(std.json.Value),
+    offset: u64,
+
+    pub fn deinit(self: *const Self, io: std.Io) void {
+        self.file.close(io);
+        self.parsed.deinit();
+        self.gpa.free(self.data);
+    }
+};
+
 pub fn open_tensor(io: std.Io, gpa: std.mem.Allocator, filename: []const u8) ![]const u8 {
     const file: std.Io.File = try std.Io.Dir.openFile(.cwd(), io, filename, .{ .mode = .read_only });
     defer file.close(io);
@@ -12,4 +29,23 @@ pub fn open_tensor(io: std.Io, gpa: std.mem.Allocator, filename: []const u8) ![]
     const t = try reader.interface.readAlloc(gpa, n);
 
     return t;
+}
+
+pub fn open(io: std.Io, gpa: std.mem.Allocator, filename: []const u8) !SafetensorsFile {
+    const file: std.Io.File = try std.Io.Dir.openFile(.cwd(), io, filename, .{ .mode = .read_only });
+    var buffer: [4096]u8 = undefined;
+    var reader = file.reader(io, &buffer);
+    const n = try reader.interface.takeInt(u64, .little);
+    const t = try reader.interface.readAlloc(gpa, n);
+    const parsed = try std.json.parseFromSlice(std.json.Value, gpa, t, .{});
+
+    const safetensors_file = SafetensorsFile{
+        .gpa = gpa,
+        .file = file,
+        .data = t,
+        .parsed = parsed,
+        .offset = 8 + n,
+    };
+
+    return safetensors_file;
 }
